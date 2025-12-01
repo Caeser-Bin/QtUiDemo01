24e011e444fba49732282f4b07da58a224415363
mlb <1437105459@qq.com>
2025-12-02 00:47:57 +0800

    feat:引入App总程序

---
+++ App/Private/Application.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Caesar on 2025/12/2.
+//
+
+#include "../Public/Application.h"
---
+++ App/Public/Application.h
@@ -0,0 +1,14 @@
+//
+// Created by Caesar on 2025/12/2.
+//
+
+#ifndef QTUIDEMO_APPLICATION_H
+#define QTUIDEMO_APPLICATION_H
+
+
+class Application
+{
+};
+
+
+#endif //QTUIDEMO_APPLICATION_H
